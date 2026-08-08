ffc935b92ae7abe2c37be51f6ba2b80cba9fe83f
Shubham Yadav <9026262332sky@gmail.com>
2026-08-08 18:03:58 +0530

    sprint proj

---
+++ SmartShip/SmartShip.IdentityService/API/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShip.IdentityService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return Ok("Identity Service is working");
+    }
+}
